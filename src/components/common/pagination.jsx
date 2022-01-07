fe07a384a68c95cd5605ad7005702c79f438ffad
satwik1608 <satwikkashyap1608@gmail.com>
2022-01-07 13:37:51 +0530

    Added Filtering

--- src/components/common/pagination.jsx
+++ src/components/common/pagination.jsx
@@ -5,7 +5,6 @@ const Pagination = (props) => {
   const { itemsCount, pageSize, currentPage, onPageChange } = props;
 
   const pageCount = Math.ceil(itemsCount / pageSize);
-  console.log(pageCount);
   if (pageCount === 1) return null;
 
   const pages = _.range(1, pageCount + 1);
@@ -15,6 +14,7 @@ const Pagination = (props) => {
       <ul className="pagination">
         {pages.map((page) => (
           <li
+            key={page}
             className={currentPage === page ? "page-item active" : "page-item"}
           >
             <a
